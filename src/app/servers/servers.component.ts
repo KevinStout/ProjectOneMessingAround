eac7f015ce55826c396511d178c656fd6c0b97ea
Kevin <stoutkev3@gmail.com>
2017-11-20 21:53:07 -0500

    adding some more components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,20 +2,31 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  //template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  constructor() { 
-    setTimeout(() =>{
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test Server';
+
+  constructor() {
+    setTimeout(() => {
       this.allowNewServer = true;
-    },2000)
+    }, 2000);
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server ' + this.serverName + ' was created';
+  }
+
+  onUpDateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
